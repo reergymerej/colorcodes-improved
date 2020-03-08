f36dda4954eea76dbf6b1b5bb4fe72575f62ccc6
Jeremy Greer <jex.grizzle@gmail.com>
2020-03-08 14:35:32 -0700

    Use shortened hex codes

--- src/components/ColorInfo.js
+++ src/components/ColorInfo.js
@@ -28,20 +28,49 @@ const RGB = ({ r = 0, g = 0, b = 0 }) => (
   </div>
 )
 
-const Hex = ({ r = 0, g = 0, b = 0 }) => (
-  <div>
-    #
-    <Red>
-      {r.toString(16)}
-    </Red>
-    <Green>
-      {g.toString(16)}
-    </Green>
-    <Blue>
-      {b.toString(16)}
-    </Blue>
-  </div>
-)
+const toHexValue = (x) => {
+  const hex = x.toString(16)
+  if (hex.length === 1) {
+    return `0${hex}`
+  }
+  return hex
+}
+
+const shouldShorten = (r, g, b) => r[0] === '0'
+  && g[0] === '0'
+  && b[0] === '0'
+
+const shortenRGB = (r, g, b) => ({
+  r: r[1],
+  g: g[1],
+  b: b[1],
+})
+
+const Hex = ({ r = 0, g = 0, b = 0 }) => {
+  r = toHexValue(r)
+  g = toHexValue(g)
+  b = toHexValue(b)
+  if (shouldShorten(r, g, b)) {
+    const short = shortenRGB(r, g, b)
+    r = short.r
+    g = short.g
+    b = short.b
+  }
+  return (
+    <div>
+      #
+      <Red>
+        {r}
+      </Red>
+      <Green>
+        {g}
+      </Green>
+      <Blue>
+        {b}
+      </Blue>
+    </div>
+  )
+}
 
 const ColorInfo = ({ r = 0, g = 0, b = 0, name = '' }) => (
   <div className="p-3 flex flex-col justify-center bg-black text-white">
